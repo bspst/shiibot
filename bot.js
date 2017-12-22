1e666253027a272dbfb5cb2ab15961ab8e8fd4d4
Hartama <hartamaadesaputra@gmail.com>
2017-12-22 16:14:54 +0700

    V2: New Feature with New Framework (#1)
    * Remove Procfile
    
    * Bot v2.0
    
    * Remove potential unfinished script
    
    * Remove unnecessary dependencies

--- bot.js
+++ bot.js
@@ -1,12 +1,25 @@
-const Telegraf = require('telegraf')
-
-
-const bot = new Telegraf(process.env.TOKEN)
-bot.start((ctx) => {
-  console.log('started:', ctx.from.id)
-  return ctx.reply('Welcome!')
+const TelegramBot = require('node-telegram-bot-api')
+const GitHubApi = require('github')
+const github = new GitHubApi({
+  debug: true
 })
-bot.hears('hi', (ctx) => ctx.reply('Hey!'))
-bot.hears('hello', (ctx) => ctx.reply('Futari Saison, Boku Wa Iya Da!'))
 
-bot.startPolling()
+const token = 'TELE_TOKEN'
+const bot = new TelegramBot(token, {polling: true})
+
+bot.onText(/\/issue ([a-zA-Z0-9 ]+)-([a-zA-Z0-9 ]+)/, (msg, match) => {
+	const chatId = msg.chat.id
+	github.authenticate({
+		type: 'token',
+		token: 'GIT_TOKEN'
+	})
+	github.issues.create({
+		owner: 'ORG',
+		repo: 'REPO',
+		title: match[1],
+		body: match[2]
+	})
+	bot.sendMessage(chatId, 'Successfully add ' + match[1])
+})
+
+console.log('Running! use [ctrl] + [c] to stop services.')
