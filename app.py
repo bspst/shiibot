683237c0982a36e6375af37944d16116079d7c6a
Hizkia Felix <hizkifw@gmail.com>
2017-12-31 15:32:41 +0700

    Test firebase database

--- app.py
+++ app.py
@@ -1,49 +1,72 @@
 import telepot
+import traceback
 import time
 import urllib3
 import os
 import firebase_admin
+from firebase_admin import credentials, db
 import json
 
 bot = telepot.Bot(os.environ['BOT_TOKEN'])
 bot.setWebhook("")
 
-cred = firebase_admin.credentials.Certificate(json.loads(os.environ['FIREBASE_KEY']))
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate(json.loads(os.environ['FIREBASE_KEY']))
+app = firebase_admin.initialize_app(cred, {
+    'databaseURL': os.environ['FIREBASE_DB_URL']
+})
 
 def handle(msg):
+    global app
+
     content_type, chat_type, chat_id = telepot.glance(msg)
     sender = msg['from']
     sender_id, sender_name = sender['id'], sender['first_name']
     print(content_type, chat_type, chat_id, sender_name)
+    print('< ', msg['text'])
 
-    if chat_id != int(os.environ['GROUP_ID']):
+    if chat_id != int(os.environ['GROUP_ID']) and chat_type == "group":
         bot.sendMessage(chat_id, "Sorry, this is a private bot.")
         bot.sendMessage(chat_id, "https://github.com/bspst/shiibot")
         return
 
-    if content_type == 'text':
-        if msg['text'][0] == '/':
-            cmd = msg['text'].split()[0][1:]
+    reply = None
+    try:
+        if content_type == 'text':
+            if msg['text'][0] == '/':
+                cmd = msg['text'].split()[0][1:]
 
-            # Check if command is tagged
-            if '@' in cmd:
-                tagged_cmd = cmd.split('@')
-                cmd = tagged_cmd[0]
-                tag = tagged_cmd[1]
-                if tag != 'shiina_mashibot':
-                    # Do nothing if not for bot
-                    return
+                # Check if command is tagged
+                if '@' in cmd:
+                    tagged_cmd = cmd.split('@')
+                    cmd = tagged_cmd[0]
+                    tag = tagged_cmd[1]
+                    if tag != 'shiina_mashibot':
+                        # Do nothing if not for bot
+                        return
 
-            if cmd == "start":
-                bot.sendMessage(chat_id, "I'm alive!")
+                if cmd == "start":
+                    reply = "I'm alive!"
 
-            if cmd == "ping":
-                bot.sendMessage(chat_id, "Pong!")
+                if cmd == "ping":
+                    reply = "Pong!"
 
-            if cmd == "fapped":
-                # TODO: Fap counter for https://github.com/bspst/todo/issues/21
-                pass
+                if cmd == "fapped":
+                    # TODO: Fap counter for https://github.com/bspst/todo/issues/21
+                    ref = db.reference("/faps", app)
+                    user_data = ref.child(str(sender_id))
+                    user_data.push(time.time())
+
+                    current = user_data.get()
+                    print(current)
+
+                    reply = "DB updated! Total: {} faps.".format(len(current))
+                    pass
+    except Exception as e:
+        reply = "```python\n{}```".format(traceback.format_exc())
+
+    if reply != None:
+        bot.sendMessage(chat_id, reply, parse_mode="Markdown", reply_to_message_id=msg['message_id'])
+        print('> ', reply)
 
 bot.message_loop(handle)
 
